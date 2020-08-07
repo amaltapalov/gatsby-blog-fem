847d0d89aa7820ee09f68006ef26a72c48c54464
Amal Tapalov <amaltapalov@gmail.com>
2020-08-07 14:43:23 +0200

    Add emotion/core

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+  <>
+    <h1>About me</h1>
+    <p>This is my personal website</p>
+    <Link to="/">&larr; Back to Home</Link>
+  </>
+);
