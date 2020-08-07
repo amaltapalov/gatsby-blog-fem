a602f03e8663b85d0f1a8820eb9b40d9e5d7ef68
Amal Tapalov <amaltapalov@gmail.com>
2020-08-07 16:43:05 +0200

    Add siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: ['gatsby-plugin-emotion'],
+  siteMetadata: {
+    title: 'Gatsby FEM Workshop',
+    description: 'A site we built together during FEM gatsby workshop',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
